65bf0a960849181befd11b3735a422b318bad019
jmperezs1 <jm.perezs1@uniandes.edu.co>
2021-09-10 11:58:42 -0500

    Cambios

--- App/view.py
+++ App/view.py
@@ -83,6 +83,14 @@ def printBestBooks(books):
 
 def printSortResults(ord_books, sample=10):
     # TODO completar modificaciones para el laboratorio 4
+    size=lt.size(ord_books)
+    if size > sample:
+        print("Los primeros ", sample, "libros ordenados son: ")
+        i=1
+        while i<= sample:
+            book = lt.getElement(ord_books,i)
+            print('Titulo: ' + book['title'] + '  ISBN: '+ 
+            book['isbn'] + '  Rating: ' + book['average_rating'])
     pass
 
 catalog = None
